bca658f226c79003a17aa0e1b32744f6a969c143
Gaurav Gupta <gaurav.gipta@innoraft.com>
2023-08-07 16:21:10 +0530

    FT2023-421: TAsk2 completed.

---
+++ web/modules/custom/config_entity_movie/src/MovieInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\config_entity_movie;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface defining a movie entity type.
+ */
+interface MovieInterface extends ConfigEntityInterface {
+
+}
